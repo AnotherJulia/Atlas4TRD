e0f9b5c02f60f12211cbf25c9ebe76b70187fabd
Julia <jbertsch2002@gmail.com>
2024-01-16 14:42:18 +0100

    update capacity

--- examples/TRD/main.py
+++ examples/TRD/main.py
@@ -6,7 +6,6 @@ general_capacity = 240
 left = 67
 
 
-
 if __name__ == '__main__':
     env = Environment(time=0, dt=1)  # dt = 1week
 
@@ -25,24 +24,24 @@ if __name__ == '__main__':
 
     # LEVEL 1 : AUGMENTED THERAPIES
 
-    env.create_step(slug="ad", description="AD Treatment, 4wks", capacity=30, config="../../config/ad_config.json",
+    env.create_step(slug="ad", description="AD Treatment, 4wks", capacity=13, config="../../config/ad_config.json",
                     depth=1, env=env)
     env.create_connection(start_slug="intake", end_slug="ad")
     env.create_connection("ad", "remission")
 
-    env.create_step(slug="ap", description="AP treatment, 4wks", capacity=20, config="../../config/ap_config.json",
+    env.create_step(slug="ap", description="AP treatment, 4wks", capacity=10, config="../../config/ap_config.json",
                     depth=1, env=env)
     env.create_connection(start_slug="intake", end_slug="ap")
     env.create_connection("ap", "remission")
 
-    env.create_step(slug="ad_ap", description="AP+AD treatment, 4wks", capacity=30,
+    env.create_step(slug="ad_ap", description="AP+AD treatment, 4wks", capacity=10,
                     config="../../config/ad_ap_config.json", depth=1, env=env)
     env.create_connection(start_slug="intake", end_slug="ad_ap")
     env.create_connection("ad_ap", "remission")
 
     # LEVEL 2: ESKETAMINE TREATMENT
 
-    env.create_step(slug="esketamine", description="Esketamine treatment, 4wks", capacity=60,
+    env.create_step(slug="esketamine", description="Esketamine treatment, 4wks", capacity=15,
                     config="../../config/esketamine_config.json", depth=2, env=env)
     env.create_connection(start_slug="ad", end_slug="esketamine")
     env.create_connection(start_slug="ap", end_slug="esketamine")
@@ -50,7 +49,7 @@ if __name__ == '__main__':
 
     # LEVEL 3: ECT
 
-    env.create_step(slug="ect", description="ECT Treatment, 28wks", capacity=15, config="../../config/ect_config.json",
+    env.create_step(slug="ect", description="ECT Treatment, 28wks", capacity=2, config="../../config/ect_config.json",
                     depth=3, env=env)
     env.create_connection("esketamine", "ect")
     env.create_connection("ect", "remission")
