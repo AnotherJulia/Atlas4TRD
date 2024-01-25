782ede88c1394ee8122b3f10ba9781496c99b57c
Julia <jbertsch2002@gmail.com>
2024-01-25 14:58:24 +0100

    meeting sims

--- examples/TRD/main.py
+++ examples/TRD/main.py
@@ -1,7 +1,4 @@
-import sys
-print(sys.path)
-
-import os
+import os, sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 
 from core import Environment, Factory, SimAnalyzer
@@ -15,11 +12,11 @@ steps = ["ad", "ap", "ad_ap", "esketamine", "ect"]
 
 capacities = {
      "total": 50,
-      "ad": 0.3,
+      "ad": 0.4,
       "ad_ap": 0.15,
-      "ap": 0.12,
-      "esk": 0.4,
-      "ect": 0.0375
+      "ap": 0.10,
+      "esk": 0.2,
+      "ect": 0.05
 }
 
 def vary_capacities(capacities):
@@ -55,32 +52,32 @@ def run_simulation(simulation_id):
 
     # LEVEL 1 : AUGMENTED THERAPIES
 
-    env.create_step(slug="ad", description="AD Treatment, 8wks", capacity=get_capacity("ad"), config="../../config/ad_config.json",
+    env.create_step(slug="ad", description="AD Treatment, 8wks", capacity=get_capacity("ad"), config="config/ad_config.json",
                     depth=1, env=env)
     env.create_connection(start_slug="intake", end_slug="ad")
     env.create_connection("ad", "remission")
 
-    env.create_step(slug="ap", description="AP treatment, 8wks", capacity=get_capacity("ap"), config="../../config/ap_config.json",
+    env.create_step(slug="ap", description="AP treatment, 8wks", capacity=get_capacity("ap"), config="config/ap_config.json",
                     depth=1, env=env)
     env.create_connection(start_slug="intake", end_slug="ap")
     env.create_connection("ap", "remission")
 
     env.create_step(slug="ad_ap", description="AP+AD treatment, 8wks", capacity=get_capacity("ad_ap"),
-                    config="../../config/ad_ap_config.json", depth=1, env=env)
+                    config="config/ad_ap_config.json", depth=1, env=env)
     env.create_connection(start_slug="intake", end_slug="ad_ap")
     env.create_connection("ad_ap", "remission")
 
     # LEVEL 2: ESKETAMINE TREATMENT
 
     env.create_step(slug="esketamine", description="Esketamine treatment, 12wks", capacity=get_capacity("esk"),
-                    config="../../config/esketamine_config.json", depth=2, env=env)
+                    config="config/esketamine_config.json", depth=2, env=env)
     env.create_connection(start_slug="ad", end_slug="esketamine")
     env.create_connection(start_slug="ap", end_slug="esketamine")
     env.create_connection(start_slug="ad_ap", end_slug="esketamine")
 
     # LEVEL 3: ECT
 
-    env.create_step(slug="ect", description="ECT Treatment, 28wks", capacity=get_capacity("ect"), config="../../config/ect_config.json",
+    env.create_step(slug="ect", description="ECT Treatment, 28wks", capacity=get_capacity("ect"), config="config/ect_config.json",
                     depth=3, env=env)
     env.create_connection("esketamine", "ect")
     env.create_connection("ect", "remission")
@@ -88,7 +85,7 @@ def run_simulation(simulation_id):
 
     # generate_networkx_graph(env)
 
-    factory = Factory(config="../../config/agent_params.json", agent_class_type=Patient)
+    factory = Factory(config="config/agent_params.json", agent_class_type=Patient)
     env.connect_factory(factory)
 
     # Set up the initial conditions of the pathway
@@ -100,10 +97,10 @@ def run_simulation(simulation_id):
 
     env.set_patient_rate(1)  # this means 1 patient a week coming into the trd pathway
 
-    env.run(until=150, verbose=False)
+    env.run(until=200, verbose=False)
 
     # env.plot_occupancies()
-    env.plot_waiting_queues()
+    env.plot_waiting_queues(steps)
 
     from core import SimulationInstance
     instance = SimulationInstance(run_id=simulation_id, agents=env.agents)
